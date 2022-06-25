0ae3da5ff949f947cf0af728c340f71eb1a72d28
Suvadra-Barua <61960537+Suvadra-Barua@users.noreply.github.com>
2022-06-26 00:06:29 +0600

    Add state variables

---
+++ DocumentVerification.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.7;
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+
+contract DocumentVerification is Ownable
+{
+    address[] verifiers;
+    struct ApplicationInfo 
+    { 
+        uint applicationId;
+        address documentOwner;
+        string description;
+        string ipfsUrl;
+        uint totalYesVote;
+        uint totalNoVote;
+        bool applicationStatus;
+    }
+    mapping(uint=>ApplicationInfo) applicationIdToApplicationInfo;
+    ApplicationInfo[] Applications;
+ }
